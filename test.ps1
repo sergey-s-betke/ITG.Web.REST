ab5a75fcd14b9c8377d09a8fc8fa1c3961817737
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-10-09 23:05:18 +0400

    * заготовка под модуль

---
+++ test.ps1
@@ -0,0 +1,13 @@
+﻿[CmdletBinding(
+	SupportsShouldProcess=$true,
+	ConfirmImpact="Medium"
+)]
+param ()
+
+Import-Module `
+    (join-path `
+        -path ( ( [System.IO.FileInfo] ( $myinvocation.mycommand.path ) ).directory ) `
+        -childPath 'ITG.Web.REST' `
+    ) `
+	-Force `
+;
